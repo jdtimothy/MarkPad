e995fc200a83106e222eb40266829f951883298d
Joshua <jdtimothy@proton.me>
2026-07-07 19:30:00 -0600

    feat: scaffold Electron app with esbuild renderer bundle

---
+++ src/main/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, '..', 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+  win.loadFile(path.join(__dirname, '..', 'renderer', 'index.html'));
+  return win;
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
---
+++ src/renderer/index.js
@@ -0,0 +1,3 @@
+import './styles.css';
+
+console.log('MarkPad renderer loaded');
